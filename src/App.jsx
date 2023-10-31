a15f61908cc0129480f7e6383d960e50c1fbb3ea
Safayet Nur <safayetnurelectra@gmail.com>
2023-10-31 08:39:19 -0700

    dynamic functionality added with logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,97 @@
 import Gallery from "./component/gallery";
+import { useState } from "react";
 
 function App() {
+  const collection = [
+    {
+      id: "1",
+      name: "Blue HeadPhone",
+      img: "https://i.ibb.co/NsvwcNG/image-1.webp",
+    },
+    {
+      id: "2",
+      name: "Green HeadPhone",
+      img: "https://i.ibb.co/SnStx09/image-2.webp",
+    },
+    {
+      id: "3",
+      name: "Pink HeadPhone",
+      img: "https://i.ibb.co/qDfjFkJ/image-3.webp",
+    },
+    {
+      id: "4",
+      name: "Red HeadPhone",
+      img: "https://i.ibb.co/JyZvxXr/image-4.webp",
+    },
+    {
+      id: "5",
+      name: "Yellow HeadPhone",
+      img: "https://i.ibb.co/P18QZsW/image-5.webp",
+    },
+    {
+      id: "6",
+      name: "White HeadPhone",
+      img: "https://i.ibb.co/pQYLJD5/image-6.webp",
+    },
+    {
+      id: "7",
+      name: "Black Watch",
+      img: "https://i.ibb.co/tJMxG6Y/image-7.webp",
+    },
+    {
+      id: "8",
+      name: "Orange Watch",
+      img: "https://i.ibb.co/RgDzyR4/image-8.webp",
+    },
+    {
+      id: "9",
+      name: "Ash Watch",
+      img: "https://i.ibb.co/BL0BThZ/image-9.webp",
+    },
+    {
+      id: "10",
+      name: "Red Wall",
+      img: "https://i.ibb.co/hCmCkk6/image-10.jpg",
+    },
+    {
+      id: "11",
+      name: "Yellow Telephone",
+      img: "https://i.ibb.co/BfdSRNP/image-11.jpg",
+    },
+  ];
+  const [checkedItems, setCheckedItems] = useState(
+    Array(collection.length).fill(false)
+  );
+  const [itemCount, setItemCount] = useState([]);
+  const handleChange = (i) => {
+    console.log("object", i);
+    const updatedCheckedItems = [...checkedItems];
+    updatedCheckedItems[i] = !updatedCheckedItems[i];
+    setCheckedItems(updatedCheckedItems);
+    console.log("object", updatedCheckedItems);
+    setItemCount(updatedCheckedItems?.filter((e, i) => e === true));
+    console.log(itemCount.length + 1);
+  };
   return (
-    <div className="bg-white max-w-7xl mx-auto border border-gray-400 shadow-lg my-4 rounded overflow-hidden">
-      <h2 className="text-xl font-bold text-black border-0 border-b py-4 px-12">
-        Gallery
-      </h2>
-      <Gallery />
+    <div className="bg-white max-w-6xl mx-auto shadow-lg my-10 rounded-lg overflow-hidden">
+      {itemCount.length ? (
+        <div className="flex justify-between items-center border-0 border-b  py-4 px-12">
+          <div className="text-xl font-bold text-black">
+            <input type="checkbox" checked /> {itemCount.length} Files Selected
+          </div>
+          <h2 className="text-red-500 font-medium">Delete files</h2>
+        </div>
+      ) : (
+        <h2 className="text-xl font-bold text-black border-0 border-b py-4 px-12">
+          Gallery
+        </h2>
+      )}
+      <Gallery
+        checkedItems={checkedItems}
+        setCheckedItems={setCheckedItems}
+        collection={collection}
+        handleChange={handleChange}
+      />
     </div>
   );
 }
--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,207 +1,33 @@
-import { useState } from "react";
-
-const Gallery = () => {
-  const [isChecked, setIsChecked] = useState(false);
-  const handleChange = () => {
-    console.log("hi");
-    setIsChecked(!isChecked);
-  };
-
+const Gallery = ({ checkedItems, collection, handleChange }) => {
   return (
     <div className="grid grid-cols-5 grid-flow-row gap-8 p-10">
-      <div className="col-span-2 row-span-2 w-full">
-        <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-          <img
-            className="w-full rounded-xl"
-            src="https://i.ibb.co/NsvwcNG/image-1.webp"
-            alt="Sunset in the mountains"
-          />
-          {isChecked ? (
+      {collection?.map((e, i) => (
+        <div
+          className={`w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2 ${
+            i % 12 === 0 ? "col-span-2 row-span-2" : "col-span-1 row-span-1"
+          }`}
+          key={i}
+        >
+          <img className="w-full rounded-xl" src={e?.img} alt={e?.name} />
+          {checkedItems[i] ? (
             <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
           ) : (
             <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
           )}
-
           <label className="invisible group-hover:visible absolute top-2 left-2">
             <input
               type="checkbox"
-              checked={isChecked}
-              onChange={handleChange}
+              checked={checkedItems[i]}
+              onChange={() => handleChange(i)}
             />
           </label>
         </div>
-      </div>
-
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-        <img
-          className="w-full rounded-xl"
-          src="https://i.ibb.co/SnStx09/image-2.webp"
-          alt="Sunset in the mountains"
-        />
-        {isChecked ? (
-          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-        ) : (
-          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
-        )}
-
-        <label className="invisible group-hover:visible absolute top-2 left-2">
-          <input type="checkbox" checked={isChecked} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-        <img
-          className="w-full rounded-xl"
-          src="https://i.ibb.co/qDfjFkJ/image-3.webp"
-          alt="Sunset in the mountains"
-        />
-        {isChecked ? (
-          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-        ) : (
-          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
-        )}
-
-        <label className="invisible group-hover:visible absolute top-2 left-2">
-          <input type="checkbox" checked={isChecked} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-        <img
-          className="w-full rounded-xl"
-          src="https://i.ibb.co/JyZvxXr/image-4.webp"
-          alt="Sunset in the mountains"
-        />
-        {isChecked ? (
-          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-        ) : (
-          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
-        )}
-
-        <label className="invisible group-hover:visible absolute top-2 left-2">
-          <input type="checkbox" checked={isChecked} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-        <img
-          className="w-full rounded-xl"
-          src="https://i.ibb.co/P18QZsW/image-5.webp"
-          alt="Sunset in the mountains"
-        />
-        {isChecked ? (
-          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-        ) : (
-          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
-        )}
-
-        <label className="invisible group-hover:visible absolute top-2 left-2">
-          <input type="checkbox" checked={isChecked} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-        <img
-          className="w-full rounded-xl"
-          src="https://i.ibb.co/pQYLJD5/image-6.webp"
-          alt="Sunset in the mountains"
-        />
-        {isChecked ? (
-          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-        ) : (
-          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
-        )}
-
-        <label className="invisible group-hover:visible absolute top-2 left-2">
-          <input type="checkbox" checked={isChecked} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-        <img
-          className="w-full rounded-xl"
-          src="https://i.ibb.co/tJMxG6Y/image-7.webp"
-          alt="Sunset in the mountains"
-        />{" "}
-        {isChecked ? (
-          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-        ) : (
-          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
-        )}
-        <label className="invisible group-hover:visible absolute top-2 left-2">
-          <input type="checkbox" checked={isChecked} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-        <img
-          className="w-full rounded-xl"
-          src="https://i.ibb.co/RgDzyR4/image-8.webp"
-          alt="Sunset in the mountains"
-        />{" "}
-        {isChecked ? (
-          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-        ) : (
-          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
-        )}
-        <label className="invisible group-hover:visible absolute top-2 left-2">
-          <input type="checkbox" checked={isChecked} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-        <img
-          className="w-full rounded-xl"
-          src="https://i.ibb.co/BL0BThZ/image-9.webp"
-          alt="Sunset in the mountains"
-        />{" "}
-        {isChecked ? (
-          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-        ) : (
-          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
-        )}
-        <label className="invisible group-hover:visible absolute top-2 left-2">
-          <input type="checkbox" checked={isChecked} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-        <img
-          className="w-full rounded-xl"
-          src="https://i.ibb.co/hCmCkk6/image-10.jpg"
-          alt="Sunset in the mountains"
-        />{" "}
-        {isChecked ? (
-          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-        ) : (
-          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
-        )}
-        <label className="invisible group-hover:visible absolute top-2 left-2">
-          <input type="checkbox" checked={isChecked} onChange={handleChange} />
-        </label>
-      </div>
-
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2">
-        <img
-          className="w-full rounded-xl"
-          src="https://i.ibb.co/BfdSRNP/image-11.jpg"
-          alt="Sunset in the mountains"
-        />{" "}
-        {isChecked ? (
-          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-        ) : (
-          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
-        )}
-        <label className="invisible group-hover:visible absolute top-2 left-2">
-          <input type="checkbox" checked={isChecked} onChange={handleChange} />
-        </label>
-      </div>
-
+      ))}
       <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-dashed border-2">
         <img
-          className="w-full rounded-xl p-20"
+          className="w-full rounded-xl p-14"
           src="https://i.ibb.co/HDfnDvm/images.png"
-          alt="Sunset in the mountains"
+          alt="Add Images"
         />{" "}
       </div>
     </div>
