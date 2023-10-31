c56a8d9ca2e2ba7cb63f88544473597c1e7552a6
Safayet Nur <safayetnurelectra@gmail.com>
2023-10-31 10:25:31 -0700

    draggable functionality added

--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,35 +1,81 @@
+import { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
 const Gallery = ({ checkedItems, collection, handleChange }) => {
+  const [characters, updateCharacters] = useState(collection);
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    console.log(result);
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateCharacters(items);
+  }
   return (
-    <div className="grid grid-cols-5 grid-flow-row gap-8 p-10">
-      {collection?.map((e, i) => (
-        <div
-          className={`w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2 ${
-            i % 12 === 0 ? "col-span-2 row-span-2" : "col-span-1 row-span-1"
-          }`}
-          key={i}
-        >
-          <img className="w-full rounded-xl" src={e?.img} alt={e?.name} />
-          {checkedItems[i] ? (
-            <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
-          ) : (
-            <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
+    <div>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="characters">
+          {(provided) => (
+            <div
+              className="grid grid-cols-5 grid-flow-row gap-8 p-10"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {characters?.map((e, index) => {
+                // console.log("object,", e.id);
+                return (
+                  <Draggable
+                    key={e?.id}
+                    draggableId={e?.id.toString()}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        className={`w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2 ${
+                          index % 12 === 0
+                            ? "col-span-2 row-span-2"
+                            : "col-span-1 row-span-1"
+                        }`}
+                      >
+                        <img
+                          className="w-full rounded-xl"
+                          src={e?.img}
+                          alt={e?.name}
+                        />
+                        {checkedItems[index] ? (
+                          <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
+                        ) : (
+                          <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
+                        )}
+                        <label className="invisible group-hover:visible absolute top-2 left-2">
+                          <input
+                            type="checkbox"
+                            checked={checkedItems[index]}
+                            onChange={() => handleChange(index)}
+                          />
+                        </label>
+                      </div>
+                    )}
+                  </Draggable>
+                );
+              })}
+
+              <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-dashed border-2">
+                <img
+                  className="w-full rounded-xl p-14"
+                  src="https://i.ibb.co/HDfnDvm/images.png"
+                  alt="Add Images"
+                />{" "}
+              </div>
+              {provided.placeholder}
+            </div>
           )}
-          <label className="invisible group-hover:visible absolute top-2 left-2">
-            <input
-              type="checkbox"
-              checked={checkedItems[i]}
-              onChange={() => handleChange(i)}
-            />
-          </label>
-        </div>
-      ))}
-      <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-dashed border-2">
-        <img
-          className="w-full rounded-xl p-14"
-          src="https://i.ibb.co/HDfnDvm/images.png"
-          alt="Add Images"
-        />{" "}
-      </div>
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
