decc56c4333aef48c66e38b6a89db57eb7b4c03e
Safayet Nur <safayetnurelectra@gmail.com>
2023-11-02 00:40:42 -0700

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,60 @@
-import Gallery from "./component/gallery";
 import { useState } from "react";
+import Gallery from "./component/gallery";
 
 function App() {
   const collection = [
     {
-      id: "1",
+      id: "0",
       name: "Blue HeadPhone",
       img: "https://i.ibb.co/NsvwcNG/image-1.webp",
     },
     {
-      id: "2",
+      id: "1",
       name: "Green HeadPhone",
       img: "https://i.ibb.co/SnStx09/image-2.webp",
     },
     {
-      id: "3",
+      id: "2",
       name: "Pink HeadPhone",
       img: "https://i.ibb.co/qDfjFkJ/image-3.webp",
     },
     {
-      id: "4",
+      id: "3",
       name: "Red HeadPhone",
       img: "https://i.ibb.co/JyZvxXr/image-4.webp",
     },
     {
-      id: "5",
+      id: "4",
       name: "Yellow HeadPhone",
       img: "https://i.ibb.co/P18QZsW/image-5.webp",
     },
     {
-      id: "6",
+      id: "5",
       name: "White HeadPhone",
       img: "https://i.ibb.co/pQYLJD5/image-6.webp",
     },
     {
-      id: "7",
+      id: "6",
       name: "Black Watch",
       img: "https://i.ibb.co/tJMxG6Y/image-7.webp",
     },
     {
-      id: "8",
+      id: "7",
       name: "Orange Watch",
       img: "https://i.ibb.co/RgDzyR4/image-8.webp",
     },
     {
-      id: "9",
+      id: "8",
       name: "Ash Watch",
       img: "https://i.ibb.co/BL0BThZ/image-9.webp",
     },
     {
-      id: "10",
+      id: "9",
       name: "Red Wall",
       img: "https://i.ibb.co/hCmCkk6/image-10.jpg",
     },
     {
-      id: "11",
+      id: "10",
       name: "Yellow Telephone",
       img: "https://i.ibb.co/BfdSRNP/image-11.jpg",
     },
@@ -62,35 +62,56 @@ function App() {
   const [checkedItems, setCheckedItems] = useState(
     Array(collection.length).fill(false)
   );
+  const [characters, updateCharacters] = useState(collection);
+  const [selectedItems, setSelectedItems] = useState([]);
   const [itemCount, setItemCount] = useState([]);
+
   const handleChange = (i) => {
-    console.log("object", i);
     const updatedCheckedItems = [...checkedItems];
     updatedCheckedItems[i] = !updatedCheckedItems[i];
     setCheckedItems(updatedCheckedItems);
-    console.log("object", updatedCheckedItems);
     setItemCount(updatedCheckedItems?.filter((e, i) => e === true));
-    console.log(itemCount.length + 1);
   };
+  // delete function
+  const handleDeleteSelectedItems = () => {
+    const updatedCharacters = characters.filter(
+      (item) => !selectedItems.includes(item.id)
+    );
+    updateCharacters(updatedCharacters);
+    setSelectedItems([]);
+  };
+
   return (
-    <div className="bg-white max-w-6xl mx-auto shadow-lg my-10 rounded-lg overflow-hidden">
-      {itemCount.length ? (
+    <div className=" bg-white lg:max-w-6xl md:max-w-4xl sm:max-w-2xl mx-auto shadow-lg my-10 rounded-lg overflow-hidden">
+      {/* header start*/}
+      {selectedItems.length ? (
         <div className="flex justify-between items-center border-0 border-b  py-4 px-12">
           <div className="text-xl font-bold text-black">
-            <input type="checkbox" checked /> {itemCount.length} Files Selected
+            <input type="checkbox" checked /> {selectedItems.length} Files
+            Selected
           </div>
-          <h2 className="text-red-500 font-medium">Delete files</h2>
+          <h2
+            className="text-red-500 font-medium cursor-pointer"
+            onClick={handleDeleteSelectedItems}
+            disabled={selectedItems.length === 0}
+          >
+            Delete files
+          </h2>
         </div>
       ) : (
         <h2 className="text-xl font-bold text-black border-0 border-b py-4 px-12">
           Gallery
         </h2>
       )}
+      {/* header end*/}
       <Gallery
-        checkedItems={checkedItems}
         setCheckedItems={setCheckedItems}
         collection={collection}
         handleChange={handleChange}
+        updateCharacters={updateCharacters}
+        characters={characters}
+        selectedItems={selectedItems}
+        setSelectedItems={setSelectedItems}
       />
     </div>
   );
--- src/component/Gallery.jsx
+++ src/component/Gallery.jsx
@@ -1,41 +1,66 @@
-import { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-const Gallery = ({ checkedItems, collection, handleChange }) => {
-  const [characters, updateCharacters] = useState(collection);
+const Gallery = ({
+  characters,
+  updateCharacters,
+  handleChange,
+  selectedItems,
+  setSelectedItems,
+}) => {
+  // drag handle function
   function handleOnDragEnd(result) {
     if (!result.destination) return;
-    console.log(result);
-    const items = Array.from(characters);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
+    const sourceIndex = result.source.index;
+    const destinationIndex = result.destination.index;
+    const sourceDroppableId = result.source.droppableId;
+    const destinationDroppableId = result.destination.droppableId;
+    if (sourceDroppableId === destinationDroppableId) {
+      const items = Array.from(characters);
+      console.log("items", items);
+      const [reorderedItem] = items.splice(sourceIndex, 1);
 
-    updateCharacters(items);
+      items.splice(destinationIndex, 0, reorderedItem);
+      updateCharacters(items);
+      console.log("itemsitems", items);
+    } else {
+      console.log(
+        "Drag and drop between different droppables is not supported in this example."
+      );
+    }
   }
+
+  // item select function
+  const handleItemSelect = (id, index) => {
+    console.log(id);
+    const selectedIndex = selectedItems.indexOf(id);
+    if (selectedIndex === -1) {
+      setSelectedItems([...selectedItems, id]);
+    } else {
+      setSelectedItems(selectedItems.filter((itemId) => itemId !== id));
+    }
+  };
+
   return (
     <div>
       <DragDropContext onDragEnd={handleOnDragEnd}>
         <Droppable droppableId="characters">
           {(provided) => (
             <div
-              className="grid grid-cols-5 grid-flow-row gap-8 p-10"
+              className="grid lg:grid-cols-5 md:grid-cols-3 sm:grid-col-1 grid-flow-row gap-8 p-10"
               {...provided.droppableProps}
               ref={provided.innerRef}
             >
               {characters?.map((e, index) => {
                 // console.log("object,", e.id);
                 return (
-                  <Draggable
-                    key={e?.id}
-                    draggableId={e?.id.toString()}
-                    index={index}
-                  >
+                  <Draggable key={e?.id} draggableId={e?.id} index={index}>
                     {(provided) => (
                       <div
+                        onClick={() => handleItemSelect(e?.id, index)}
                         ref={provided.innerRef}
                         {...provided.draggableProps}
                         {...provided.dragHandleProps}
-                        className={`w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2 ${
+                        className={`w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-500 ${
                           index % 12 === 0
                             ? "col-span-2 row-span-2"
                             : "col-span-1 row-span-1"
@@ -46,7 +71,7 @@ const Gallery = ({ checkedItems, collection, handleChange }) => {
                           src={e?.img}
                           alt={e?.name}
                         />
-                        {checkedItems[index] ? (
+                        {selectedItems.includes(e?.id) ? (
                           <div className="w-full h-full bg-gray-400 opacity-20 absolute top-0 left-0"></div>
                         ) : (
                           <div className="w-full rounded-xl overflow-hidden shadow-lg cursor-pointer group bg-black opacity-0 group-hover:h-full group-hover:opacity-20 duration-500 invisible group-hover:visible absolute top-0 left-0"></div>
@@ -54,7 +79,7 @@ const Gallery = ({ checkedItems, collection, handleChange }) => {
                         <label className="invisible group-hover:visible absolute top-2 left-2">
                           <input
                             type="checkbox"
-                            checked={checkedItems[index]}
+                            checked={selectedItems.includes(e?.id)}
                             onChange={() => handleChange(index)}
                           />
                         </label>
@@ -64,6 +89,7 @@ const Gallery = ({ checkedItems, collection, handleChange }) => {
                 );
               })}
 
+              {/* add image icon */}
               <div className="w-full rounded-xl overflow-hidden shadow-lg relative cursor-pointer group border-dashed border-2">
                 <img
                   className="w-full rounded-xl p-14"
